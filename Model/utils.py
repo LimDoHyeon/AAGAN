db2652b877332c4a4131508d35cf3e972f12b12b
임도현 <94499717+LimDoHyeon@users.noreply.github.com>
2024-07-17 12:18:19 +0900

    [Feat] utils for model

---
+++ Model/utils.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import torch
+from torch.nn.utils import weight_norm
+
+
+def init_weights(m, mean=0.0, std=0.01):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        m.weight.data.normal_(mean, std)
+
+
+def get_padding(kernel_size, dilation=1):
+    return int((kernel_size*dilation - dilation) / 2)
