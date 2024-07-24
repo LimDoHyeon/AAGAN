b27aea86cd7131593e56e2a067058ec0793e87b0
임도현 <94499717+LimDoHyeon@users.noreply.github.com>
2024-07-24 12:32:49 +0900

    [Feat] data denorm

--- Model/utils.py
+++ Model/utils.py
@@ -1,8 +1,23 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import os
 import torch
-from torch.nn.utils import weight_norm
+
+
+def data_denorm(data, avg, std):
+    std = std.type(torch.cuda.FloatTensor)
+    avg = avg.type(torch.cuda.FloatTensor)
+
+    # if std == 0, change to 1.0 for nothing happen
+    std = torch.where(std == torch.tensor(0, dtype=torch.float32).cuda(),
+                      torch.tensor(1, dtype=torch.float32).cuda(),
+                      std)
+
+    # change the size of std and avg
+    std = torch.permute(std.repeat(data.shape[1], data.shape[2], 1), [2, 0, 1])
+    avg = torch.permute(avg.repeat(data.shape[1], data.shape[2], 1), [2, 0, 1])
+
+    data = torch.mul(data, std) + avg
+
+    return data
 
 
 def init_weights(m, mean=0.0, std=0.01):
@@ -13,3 +28,6 @@ def init_weights(m, mean=0.0, std=0.01):
 
 def get_padding(kernel_size, dilation=1):
     return int((kernel_size*dilation - dilation) / 2)
+
+
+
